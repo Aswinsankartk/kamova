7764202c4d209ccfa5626d03998db857cfbffe50
Aswinsankartk <164378656+Aswinsankartk@users.noreply.github.com>
2026-02-09 04:30:40 +0000

    Rewrite the whole styles and change the classes accordingly.
    Made it responsive.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const apiKey="c5de33d9f319b1b43f5b3e5010c2fc89";
 
 function getWeatherData(){
-    const city=document.querySelector(".textbox").value;
+    const city=document.querySelector(".city").value;
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(response=>response.json()).then(data=>{
-        document.querySelector(".output").innerHTML=`
+        document.querySelector(".result").innerHTML=`
         <h3>${data.name}</h3>
         <p>Temperature: ${data.main.temp} °C</p>
         <p>Humidity: ${data.main.humidity}%</p>
         <p>Weather: ${data.weather[0].description}</p>
         `;
     }).catch(()=>{
-        document.querySelector(".output").innerHTML="City not found!";
+        document.querySelector(".result").innerHTML="City not found!";
     });
 };
